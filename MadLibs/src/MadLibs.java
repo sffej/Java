b752ef7be1d1d1af595bef6f885491953d0c2b4c
Erik Sjaastad <spudlogic@gmail.com>
2013-03-15 22:15:42 -0700

    updated MadLibs
    The cursor doesn't go to the ready position when it asks for the next
    word and there isn't an exit command :(

--- MadLibs/src/MadLibs.java
+++ MadLibs/src/MadLibs.java
@@ -7,74 +7,130 @@ import java.util.Scanner;
 public class MadLibs
 {
 	 //declare private static variables
-	private static String userinput;
-	//consoleReader
-	//ltcarrot = "<"
-	//rtcarrot = ">"
-	//file
-	//String line
-	private static Scanner consoleReader;
+	private String userInput;
+	private boolean play = true;
+	public PrintWriter fileWriter;
+	private Scanner fileReader;
+	private Scanner consoleReader;
+	public String line;
 	
+
     public static void main(String[] args)
     {
     	MadLibs ml = new MadLibs();
-    	
-        consoleReader = new Scanner(System.in);
-        System.out.print("Welcome to the game of Mad Libs.\nYou will help create a story by providing words and phrases.");
-        System.out.print("(C)reate, (V)iew, or (Q)iut: ");
-        String choiceCVQ = consoleReader.nextLine().toLowerCase();
-        
-        
-       
-        if(choiceCVQ.equals("q"))
-        {
-            System.out.print("Goodbye :(");
-            System.exit(0);
-        }
-       
-        if(choiceCVQ.equals("v"))
-        {
-            System.out.print("Enter File Name: ");
-            ml.viewFile();
-        }
-       
-       
-        if(choiceCVQ.equals("c"))
-        {
-            System.out.print("Enter File Name: ");
-            ml.makeFile();
-        }
+    	ml.play();
     }
+    
+	private void play() 
+	{
+        System.out.print("Welcome to the game of Mad Libs.\nYou will help create a story by providing words and phrases.");
+        
+        while (play == true)
+        {
+	        System.out.print("(C)reate, (V)iew, or (Q)uit: ");
+	        userInput = consoleReader.nextLine();
+	        
+	        if(userInput.equalsIgnoreCase("q"))
+	        {
+	            System.out.print("Goodbye :(");
+	            play = false;
+	        }
+	       
+	        if(userInput.equalsIgnoreCase("v"))
+	        {
+	        	System.out.print("clothes, dance, simple, tarzan");
+	        	System.out.print("Choose one of the files above: ");
+	        	userInput  = consoleReader.nextLine().toLowerCase();
+	            viewFile();
+	        }
+	       
+	       
+	        if(userInput.equalsIgnoreCase("c"))
+	        {
+	        	System.out.print("clothes, dance, simple, tarzan");
+	            System.out.print("Choose one of the files above: ");
+	            userInput  = consoleReader.nextLine().toLowerCase();
+	            makeFile();
+	        }
+        }//end while loop
+	}
+
+	public MadLibs()
+	{
+		
+		consoleReader = new Scanner(System.in);
+	}
     
     public void makeFile()
     {
+    	//read file
     	File file = new File("input/" + userInput + ".txt");
-    	PrintWriter fileWriter = null;
+    	
         try
         {
-            fileWriter = new PrintWriter(madLibs1a);
+        	fileReader = new Scanner(file);
         }
-       
         catch(FileNotFoundException e)
         {
-            System.out.println(e.getMessage());
-            System.exit(0);
+            System.out.println("Not a valid file name. Please try again");
+            System.out.print("Enter File Name: ");
         }
-       
+     
+       //write file
+        File newFile = new File("output/" + userInput + ".txt");
+        try
+        {
+        	//FileOutputStream outFile = new FileOutputStream(newFile);
+        	fileWriter = new PrintWriter(newFile);
+        }
+        catch(Exception e)
+        {
+        	System.out.println("Not a valid file name. Please try again");
+        	System.exit(0);
+        }
+        
+        //fileWriter = null;
+        int leftCarrot = 0;
+        int rightCarrot = 0;
         while(fileReader.hasNextLine())
         {
-            String line = fileReader.nextLine();
-            line = replace("noun", nNoun);
-            line = replace("adjective", nAdective);
+        	line = fileReader.nextLine();
+        	do
+        	{
+        		leftCarrot = -1;
+	        	rightCarrot = -1;
+	        	leftCarrot = line.indexOf("<");
+	        	rightCarrot = line.indexOf(">");
+	        	
+	        	if(leftCarrot >= 0 && rightCarrot >= 0)
+	        	{
+	        		String wordToChange = line.substring(leftCarrot +1, rightCarrot);
+	        		String newWord = getWord(wordToChange);
+	        		line = line.replaceAll("<" + wordToChange + ">", newWord);
+	        	}
+        	}
+        	while(leftCarrot != -1 && rightCarrot != -1);
+        	System.out.println(line);
+            fileWriter.println(line);
         }
+        fileWriter.close();
+        fileReader.close();
     }
     
-    public void viewFile()
+    private String getWord(String wordToChange)
     {
+		System.out.print("enter a " + wordToChange + ": ");
+		String word = consoleReader.nextLine();
+		return word;
+	}
+
+	public void viewFile()
+    {
+    	File file = new File("output/" + userInput + ".txt");
     	Scanner fileReader = null;
         try
         {
-            fileReader = new Scanner(madLibs1a);
+            fileReader = new Scanner(file);
         }
        
         catch(FileNotFoundException e)
